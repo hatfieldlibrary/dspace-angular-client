9c9e3abb0cb905f7be745c532cf40899d0d9c564
mspalti <mspalti@gmail.com>
2016-04-29 11:29:37 -0700

    feat: Adjustments to dialog and handle item layouts and colors.
    Now using material theme background colors in the dialog.  Removed unused css rules in styles.css.  Updated typeography and layouts in the item views (still need to test layouts in IE, but changes are minor enough to pass).
    
    Issues: AC-617

--- app/public/client/app/app.js
+++ app/public/client/app/app.js
@@ -141,7 +141,7 @@ var dspaceApp = angular.module('dspaceApp', [
           'default': '500', // by default use shade 400 from the pink palette for primary intentions
           'hue-1': '300', // use shade 100 for the <code>md-hue-1</code> class
           'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
-          'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+          'hue-3': '700' // use shade 900 for the <code>md-hue-3</code> class
         })
         .accentPalette('Accent Palette', {
           'default': '500',
